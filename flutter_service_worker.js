fa9c1b52d75016efecbd084ac5922158935f774d
Yuvaraj2244 <yuvarajkkyuvaraj@gmail.com>
2025-08-27 20:09:42 +0530

    Ignore build folder

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -53,7 +53,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d99e5b8c5b8c66e9ed4faf186a673939
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "8f510430b71dde66b09a04df23021616",
+"flutter_bootstrap.js": "bb9135b410e0240feab786de3a1555ac",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
