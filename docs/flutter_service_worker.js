b01cfdc2ff4027215f037596c14e7264de3fa867
Yuvaraj2244 <yuvarajkkyuvaraj@gmail.com>
2025-08-29 22:01:51 +0530

    Fix deployment and loading screen

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -54,7 +54,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d707d29cb67b0dceeb687948ceda190b
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "4f54d6b6783ff0bcfe699413ea19a294",
+"flutter_bootstrap.js": "4e3b1a70f642fc3cf40ee878124020ad",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
